3c9e2a7cbc6aebd6b5f81ff94d6e32de8667c1b7
adarsh.haridas <adarsh.haridas@urolime.com>
2024-03-11 15:05:23 +0530

    updated image to app2 and the mapping to /app2

--- src/main/java/com/devops/coach/StartApplication.java
+++ src/main/java/com/devops/coach/StartApplication.java
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class StartApplication {
 
-    @GetMapping("/app1")
+    @GetMapping("/app2")
     public String index(final Model model) {
         model.addAttribute("title", "Test SpringBoot Deployment using Helm");
-        model.addAttribute("msg", "Hello All..We are testing springboot application deployment with Helm and Jenkins Pipeline!!!! app1 ");
+        model.addAttribute("msg", "Hello All..We are testing springboot application deployment with Helm and Jenkins Pipeline!!!! app2 ");
         return "index";
     }
 
